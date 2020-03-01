fd2d3ff00c5a2c3f83992f30e400691321aad2ee
curtaincall888 <kota3730@gmail.com>
2020-03-01 17:01:30 +0900

    メッセージ非同期通信化完了

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ChatSpace
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
+    config.time_zone = 'Tokyo'
     # -- all .rb files in that directory are automatically loaded.
     config.generators do |g|
       g.stylesheets false
